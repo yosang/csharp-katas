58bc4f474f977857752a9e6f3b2bb30ddda1e8e1
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-12 18:57:59 +0100

    solve form the minimum

---
+++ 7kyu/3.form-the-minimum/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+
+// We want to remove duplicates
+// Sort the array
+
+int[] test1 = { 1, 3, 1 };
+int[] test2 = { 5, 7, 5, 9, 7 };
+int[] test3 = { 1, 9, 3, 1, 7, 4, 6, 6, 7 };
+
+WriteLine(removeDupesAndSort(test1));
+WriteLine(removeDupesAndSort(test2));
+WriteLine(removeDupesAndSort(test3));
+
+long removeDupesAndSort(int[] a)
+{
+    Array.Sort(a); // Sorts the array
+
+    List<int> result = new List<int>(); // A list can dynamically grow/shrink in size
+    // int[] result = new int[a.Length]; // A normal array wont work because it must be initiated with a fixed size
+
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (!result.Contains(a[i]))
+        {
+            result.Add(a[i]);
+        }
+    }
+
+    return long.Parse(string.Join("", result));
+}
---
+++ 7kyu/3.form-the-minimum/readme.md
@@ -0,0 +1,11 @@
+kata link: https://www.codewars.com/kata/5ac6932b2f317b96980000ca
+
+# Task
+
+Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers in the range of 1 to 9 will be passed to the function.
+
+```
+[1, 3, 1] ==> 13
+[5, 7, 5, 9, 7] ==> 579
+[1, 9, 3, 1, 7, 4, 6, 6, 7]  ==> 134679
+```
