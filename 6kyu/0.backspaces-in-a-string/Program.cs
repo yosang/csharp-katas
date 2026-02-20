d71da944392398668a951c32386855009fa82311
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-20 11:16:46 +0100

    refactor: add comments

--- 6kyu/0.backspaces-in-a-string/Program.cs
+++ 6kyu/0.backspaces-in-a-string/Program.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.Text;
 
-namespace Program
+namespace BackspaceStringProcessor
 {
     public class App
     {
-        public class TextCannotBeEmpty : Exception
+        public class TextCannotBeEmptyException : Exception
         {
-            public TextCannotBeEmpty(string message) : base(message) { }
+            public TextCannotBeEmptyException(string message) : base(message) { }
         }
 
-        // Stack method
+        /// <summary>
+        /// Stack method - using System.Collections.Generic
+        /// Processes a string where '#' acts as a backspace character
+        /// Uses expliciti Stack<char> approach.
+        /// </summary>
         public static string ProcessBackspaces(string text, char symbol = '#')
         {
             // No point in processing an empty string, so just throw an exception if its empty before moving on
-            if (text.Length < 1) throw new TextCannotBeEmpty("Text cannot be empty");
+            if (text.Length < 1) throw new TextCannotBeEmptyException("Input text cannot be empty");
 
             Stack<char> myStack = new Stack<char>(); // We are using a stack for the backspacing = everytime we encounter a symbol, we pop from the stack, otherwise push
 
@@ -26,22 +30,32 @@ namespace Program
                 {
                     myStack.Push(c);
                 }
-                else
+                else if (myStack.Count > 0) // Make sure we pop only if there are items in the stack
                 {
-                    if (myStack.Count > 0) myStack.Pop(); // Make sure we pop only if there are items in the stack
+                    myStack.Pop();
                 }
             }
 
             // We can reverse a stack by creating a new stack from the original stack
+            Stack<char> reversed = new Stack<char>(myStack);
 
-            return string.Join("", new Stack<char>(myStack));
+            // We can also use the stack.Reverse, and convert it to an array
+            char[] reversedArr = myStack.Reverse().ToArray();
+
+            // We can join the arr back with string.Join
+            // But when array only contains chars, we can just use new string
+            string result = new string(reversedArr);
+
+            return result;
         }
 
-        // StringBuilder method
-        // We can achieve the same thing using string builder, which behaves like a stack, but specifically for building strings out of chars.
+        /// <summary>
+        /// StringBuilder method - Using System.Text
+        /// 
+        /// </summary>
         public static string ProcessBackspacesSB(string text, char symbol = '#')
         {
-            if (text.Length < 1) throw new TextCannotBeEmpty("Text cannot be empty");
+            if (text.Length < 1) throw new TextCannotBeEmptyException("Input text cannot be empty");
 
             StringBuilder sb = new StringBuilder();
 
@@ -68,15 +82,15 @@ namespace Program
             // string s4 = "";
 
             // Solution with a Stack:
-            // Console.WriteLine(ProcessBackspaces(s1));
+            Console.WriteLine(ProcessBackspaces(s1));
             // Console.WriteLine(ProcessBackspaces(s2)); // should return and empty string
             // Console.WriteLine(ProcessBackspaces(s3));
             // Console.WriteLine(ProcessBackspaces(s4)); // throws an exception
 
             // Solution with a StringBuilder
-            Console.WriteLine(ProcessBackspacesSB(s1));
-            Console.WriteLine(ProcessBackspacesSB(s2));
-            Console.WriteLine(ProcessBackspacesSB(s3));
+            // Console.WriteLine(ProcessBackspacesSB(s1));
+            // Console.WriteLine(ProcessBackspacesSB(s2));
+            // Console.WriteLine(ProcessBackspacesSB(s3));
             // Console.WriteLine(ProcessBackspacesSB(s4));
         }
     }
